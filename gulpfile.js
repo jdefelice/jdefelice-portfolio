fce3a1bc7090891268cd284878b9cd81afc52a9c
jdefelice <jonnywithnoh@gmail.com>
2015-05-18 21:59:54 +0100

    New Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,59 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
-var minifycss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var gzip = require('gulp-gzip');
-var livereload = require('gulp-livereload');
+/*!
+ * gulp
+ npm install gulp-ruby-sass gulp-autoprefixer gulp-minify-css gulp-concat gulp-uglify gulp-notify gulp-rename gulp-livereload --save-dev
+ */
 
-/* Source Paths */
-var srcSass = 'src/sass'
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    livereload = require('gulp-livereload');
 
-/* Destination Paths */
-var distCSS = 'dist/css';
-
-var gzip_options = {
-    threshold: '1kb',
-    gzipOptions: {
-        level: 9
-    }
-};
-
-/* Compile Our Sass */
-gulp.task('sass', function() {
-    return gulp.src(srcSass+'/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest(distCSS))
-        .pipe(rename({suffix: '.min'}))
+// Styles
+gulp.task('styles', function() {
+    return sass('src/sass/main.scss', { style: 'expanded' })
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(minifycss())
-        .pipe(gulp.dest(distCSS))
-        .pipe(gzip(gzip_options))
-        .pipe(gulp.dest(distCSS))
-        .pipe(livereload());
+        .pipe(gulp.dest('dist/css'))
+        .pipe(notify({ message: 'Styles task complete' }));
 });
 
-/* Watch Files For Changes */
+// Scripts
+gulp.task('scripts', function() {
+    return gulp.src('src/js/**/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// Default task
+gulp.task('default', function() {
+    gulp.start('styles', 'scripts');
+});
+
+// Watch
 gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch(srcSass+'/*.scss', ['sass']);
-});
 
-gulp.task('default', ['sass', 'watch']);
+    // Watch .scss files
+    gulp.watch('src/sass/**/*.scss', ['styles']);
+
+    // Watch .js files
+    gulp.watch('src/js/**/*.js', ['scripts']);
+
+    // Create LiveReload server
+    livereload.listen();
+
+    // Watch any files in dist/, reload on change
+    gulp.watch(['dist/**']).on('change', livereload.changed);
+
+});
